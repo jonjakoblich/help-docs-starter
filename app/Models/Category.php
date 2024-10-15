30fe6faf318e2a57080f77e75b83b2bb63d1d86a
Jon Jakoblich <jonjakoblich@me.com>
2024-10-15 08:01:58 -0500

    Categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,9 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(Category::class,'parent_id');
+    }
+
+    public function articles(): BelongsToMany
+    {
+        return $this->belongsToMany(Article::class);
+    }
 }
---
+++ tests/Feature/Models/CategoryTest.php
@@ -0,0 +1,62 @@
+<?php
+
+use App\Models\Article;
+use App\Models\Category;
+use Illuminate\Support\Str;
+
+uses()->group('models','article');
+
+beforeEach(function() {
+    $this->category = Category::factory()
+        ->hasArticles(3)
+        ->create();
+});
+
+it('has a name', function () {
+    expect($this->category)->name
+        ->not->toBeNull()
+        ->toBeString();
+});
+
+it('has a slug', function () {
+    expect($this->category)->slug
+        ->not->toBeNull()
+        ->toBeString()
+        ->toBe(Str::slug($this->category->name));
+});
+
+it('can have a parent category', function () {
+    $category = Category::factory()
+        ->forParent()
+        ->create();
+
+    expect($category)->parent
+        ->not->toBeNull()
+        ->toBeInstanceOf(Category::class);
+});
+
+it('can be without a parent', function () {
+    expect($this->category)->parent->toBeNull();
+});
+
+it('can have many children categories', function () {
+    $category = Category::factory()
+        ->hasChildren(3)
+        ->create();
+    
+    expect($category)->children
+        ->not->toBeNull()
+        ->toBeIterable()
+        ->each->toBeInstanceOf(Category::class);
+});
+
+it('can be without children', function () {
+    expect($this->category)->children->toBeEmpty();
+});
+
+it('can have many articles', function () {
+    expect($this->category)->articles
+        ->not->toBeNull()
+        ->toBeIterable()
+        ->each->toBeInstanceOf(Article::class);
+});
