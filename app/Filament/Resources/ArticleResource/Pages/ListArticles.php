d9334578d129d02ed6fd12cbf8ae720f73349c87
Jon Jakoblich <jonjakoblich@me.com>
2024-10-15 14:49:39 -0500

    Add tabs

--- app/Filament/Resources/ArticleResource/Pages/ListArticles.php
+++ app/Filament/Resources/ArticleResource/Pages/ListArticles.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\ArticleResource\Pages;
 
 use App\Filament\Resources\ArticleResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListArticles extends ListRecords
 {
@@ -14,6 +16,19 @@ class ListArticles extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'published' => Tab::make()
+            ->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'published')),
+            'draft' => Tab::make()
+            ->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'draft')),
+            'private' => Tab::make()
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'private')),
         ];
     }
 }
