aa4d6764ec765307633d4bc75c3f4de280774c49
Jon Jakoblich <jonjakoblich@me.com>
2024-10-21 17:01:50 -0500

    Rough out homepage

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,7 +1,35 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-    
+    <header class="flex justify-between">
+        <div>
+            <!-- Company logo -->
+        </div>
+        <div>
+            <!-- Link back to product homepage -->
+        </div>
+    </header>
+    <div class="">
+        <h1>Help is only a click away</h1>
+        <form>
+            <input type="text" />
+        </form>
+    </div>
+    <div class="grid grid-cols-3">
+        <!-- Pinned Categories -->
+    </div>
+     <div>
+         <!-- Pinned/Frequent Articles -->
+     </div>
 </template>
+
+<script setup lang="ts">
+import {useForm} from '@inertiajs'
+
+defineProps([
+    pinnedCategories: Array,
+    pinnedArticles: Array,
+])
+
+const searchForm = useForm({
+    keywords: ''
+})
+</script>
