9a1b20daa80aab56728903298b046e98054416f5
Jon Jakoblich <jonjakoblich@me.com>
2024-10-23 15:00:17 -0500

    Add basic featured categories and articles styling

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -8,18 +8,28 @@
              <Link href="#">{{productHomeName ?? 'Product homepage'}}</Link>
         </div>
     </header>
-    <div class="flex flex-col place-items-center bg-indigo-600 w-full">
+    <div class="flex flex-col place-items-center bg-indigo-600 w-full py-6">
         <h1 class="text-white text-2xl m-3 font-bold">Help is only a click away</h1>
         <form class="h-20">
             <!-- Debounced search input -->
             <input type="text" class="rounded-md w-96" placeholder="Type your question..." />
         </form>
     </div>
-    <div class="grid grid-cols-3">
-        <!-- Pinned Categories -->
+    <div class="grid grid-cols-3 gap-2 m-2 w-1/2 mx-auto">
+        <!-- Featured Categories -->
+        <Link v-for="category in featuredCategories"  :href="route('category.view',category.slug)">
+            <p class="border-indigo-500 border-2 rounded-md text-center text-indigo-500 text-xl p-4 hover:text-indigo-700 hover:border-indigo-700">
+                {{ category.name }}
+            </p>
+        </Link>
     </div>
-     <div>
-         <!-- Pinned/Frequent Articles -->
+     <div class="w-1/2 mx-auto">
+         <!-- Featured Articles -->
+        <ul>
+            <li v-for="article in featuredArticles">
+                <Link :href="route('article.view',article.slug)">{{ article.name }}</Link>
+            </li>
+        </ul>
      </div>
 </template>
 
@@ -37,8 +47,8 @@ interface Article {
 }
 
 defineProps({
-    pinnedCategories: Array<Category>,
-    pinnedArticles: Array<Article>,
+    featuredCategories: Array<Category>,
+    featuredArticles: Array<Article>,
     productHomeUrl: String,
     productHomeName: String,
 })
