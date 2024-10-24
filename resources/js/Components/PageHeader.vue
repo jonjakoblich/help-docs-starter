ee874df31bb07c8bd65506065e5c74af3659e5cc
Jon Jakoblich <jonjakoblich@me.com>
2024-10-23 22:29:04 -0500

    Add support center, add search bar

--- resources/js/Components/PageHeader.vue
+++ resources/js/Components/PageHeader.vue
@@ -4,9 +4,13 @@ import ProductHomeLink from './ProductHomeLink.vue';
 </script>
 
 <template>
-    <header class="flex justify-between w-full mx-auto place-items-center py-1">
-        <div>
+    <header class="flex justify-between w-full mx-auto place-items-center py-2">
+        <div class="flex place-items-center gap-2">
              <ApplicationLogo class="size-12" />
+             <p>Support Center</p>
+        </div>
+        <div>
+            <input type="text" class="rounded-md h-10 border-gray-300 w-96" placeholder="Search support docs..." />
         </div>
         <div>
             <ProductHomeLink />
