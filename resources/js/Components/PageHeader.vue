7525f041969a71138b5fdcf28f6f262ade6bdff2
Jon Jakoblich <jonjakoblich@me.com>
2024-10-24 20:33:34 -0500

    Search modal

--- resources/js/Components/PageHeader.vue
+++ resources/js/Components/PageHeader.vue
@@ -3,8 +3,11 @@ import { Link } from '@inertiajs/vue3';
 import ApplicationLogo from './ApplicationLogo.vue';
 import ProductHomeLink from './ProductHomeLink.vue';
 import { ref } from 'vue';
+import SearchModal from './SearchModal.vue';
 
 const currentPath = ref(window.location.href)
+
+const searchModalVisible = ref(false)
 </script>
 
 <template>
@@ -16,7 +19,22 @@ const currentPath = ref(window.location.href)
             </Link>
         </div>
         <div v-if="currentPath != route('home') + '/'">
-            <input type="text" class="rounded-md h-10 border-gray-300 w-96" placeholder="Search support docs..." />
+            <button 
+                type="button" 
+                class="group flex h-6 w-6 items-center justify-center sm:justify-start md:h-auto md:w-80 md:flex-none md:rounded-lg md:py-2.5 md:pl-4 md:pr-3.5 md:text-sm md:ring-1 md:ring-slate-200 md:hover:ring-slate-300 lg:w-96 dark:md:bg-slate-800/75 dark:md:ring-inset dark:md:ring-white/5 dark:md:hover:bg-slate-700/40 dark:md:hover:ring-slate-500"
+                @click="searchModalVisible = !searchModalVisible"
+            >
+                <svg aria-hidden="true" viewBox="0 0 20 20" class="h-5 w-5 flex-none fill-slate-400 group-hover:fill-slate-500 md:group-hover:fill-slate-400 dark:fill-slate-500">
+                    <path d="M16.293 17.707a1 1 0 0 0 1.414-1.414l-1.414 1.414ZM9 14a5 5 0 0 1-5-5H2a7 7 0 0 0 7 7v-2ZM4 9a5 5 0 0 1 5-5V2a7 7 0 0 0-7 7h2Zm5-5a5 5 0 0 1 5 5h2a7 7 0 0 0-7-7v2Zm8.707 12.293-3.757-3.757-1.414 1.414 3.757 3.757 1.414-1.414ZM14 9a4.98 4.98 0 0 1-1.464 3.536l1.414 1.414A6.98 6.98 0 0 0 16 9h-2Zm-1.464 3.536A4.98 4.98 0 0 1 9 14v2a6.98 6.98 0 0 0 4.95-2.05l-1.414-1.414Z"></path>
+                </svg>
+                <span class="sr-only md:not-sr-only md:ml-2 md:text-slate-500 md:dark:text-slate-400">Search support docs</span>
+            </button>
+            <Teleport to="body">
+                <SearchModal 
+                    v-if="searchModalVisible" 
+                    @close-modal="searchModalVisible = false"
+                />
+            </Teleport>
         </div>
         <div>
             <ProductHomeLink />
---
+++ resources/js/Components/SearchModal.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="fixed inset-0 z-50 mx-auto" @click="(e) => maybeCloseModal(e)" aria-modal="true">
+        <div class="fixed inset-0 bg-slate-900/50 backdrop-blur" ></div>
+        <div class="fixed inset-0 overflow-y-auto px-4 py-4 sm:px-6 sm:py-20 md:py-32 lg:px-8 lg:py-[15vh]">
+            <div id="searchModal" class="flex flex-col p-3 mx-auto transform-gpu overflow-hidden rounded-xl bg-white shadow-xl sm:max-w-xl dark:bg-slate-800 dark:ring-1 dark:ring-slate-700">
+                <div class="flex place-items-center gap-1">
+                    <svg aria-hidden="true" viewBox="0 0 20 20" class="pointer-events-none h-full w-5 fill-slate-400 dark:fill-slate-500">
+                        <path d="M16.293 17.707a1 1 0 0 0 1.414-1.414l-1.414 1.414ZM9 14a5 5 0 0 1-5-5H2a7 7 0 0 0 7 7v-2ZM4 9a5 5 0 0 1 5-5V2a7 7 0 0 0-7 7h2Zm5-5a5 5 0 0 1 5 5h2a7 7 0 0 0-7-7v2Zm8.707 12.293-3.757-3.757-1.414 1.414 3.757 3.757 1.414-1.414ZM14 9a4.98 4.98 0 0 1-1.464 3.536l1.414 1.414A6.98 6.98 0 0 0 16 9h-2Zm-1.464 3.536A4.98 4.98 0 0 1 9 14v2a6.98 6.98 0 0 0 4.95-2.05l-1.414-1.414Z"></path>
+                    </svg>
+                    <input
+                        v-model="keywords"
+                        type="text" 
+                        class="border-0 w-full text-xl h-12 focus:ring-0"
+                        placeholder="Search support docs..."
+                        @input="debounce(search,500)"
+                        autofocus
+                        tabindex="0"
+                    />
+                </div>
+                <ul id="results">
+                    <li v-for="article in results" class="p-2 text-lg">
+                        <Link :href="route('article.view',article.slug)" class="text-indigo-500 hover:text-indigo-700">{{ article.name }}</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Article } from '@/types';
+import { Link } from '@inertiajs/vue3';
+import axios from 'axios';
+import { ref } from 'vue';
+
+const emit = defineEmits(['closeModal'])
+
+const keywords = ref('')
+const results = ref<Array<Article>>()
+
+const debounce = (fn: Function, delay: number) => {
+    setTimeout(() => {
+        fn();
+    }, delay);
+};
+
+const search = () => {
+    axios
+        .post(route('search.retrieve'),{
+            s: keywords.value
+        })
+        .then((res) => {
+            console.log(res)
+            results.value = res.data
+        })
+        .catch((err) => console.error(err))
+}
+
+function maybeCloseModal(e: Event) {
+    if(!e.target?.closest('#searchModal')) {
+        emit('closeModal')
+    }
+}
+</script>
