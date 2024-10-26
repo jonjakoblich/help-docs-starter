e48f51add7ae4691b2dc81e3f47f5ed826a8a90a
Jon Jakoblich <jonjakoblich@me.com>
2024-10-25 21:31:08 -0500

    Refactor

--- resources/js/Components/PageHeader.vue
+++ resources/js/Components/PageHeader.vue
@@ -1,15 +1,3 @@
-<script setup lang="ts">
-import { Link } from '@inertiajs/vue3';
-import ApplicationLogo from './ApplicationLogo.vue';
-import ProductHomeLink from './ProductHomeLink.vue';
-import { ref } from 'vue';
-import SearchModal from './SearchModal.vue';
-
-const currentPath = ref(window.location.href)
-
-const searchModalVisible = ref(false)
-</script>
-
 <template>
     <header class="flex justify-between w-full mx-auto place-items-center py-2">
         <div>
@@ -21,13 +9,11 @@ const searchModalVisible = ref(false)
         <div v-if="currentPath != route('home') + '/'">
             <button 
                 type="button" 
-                class="group flex h-6 w-6 items-center justify-center sm:justify-start md:h-auto md:w-80 md:flex-none md:rounded-lg md:py-2.5 md:pl-4 md:pr-3.5 md:text-sm md:ring-1 md:ring-gray-200 md:hover:ring-gray-300 lg:w-96 dark:md:bg-gray-800/75 dark:md:ring-inset dark:md:ring-white/5 dark:md:hover:bg-gray-700/40 dark:md:hover:ring-gray-500"
+                class="group flex h-6 w-6 items-center justify-center sm:justify-start md:h-auto md:w-80 md:flex-none md:rounded-lg md:py-2.5 md:pl-4 md:pr-3.5 md:text-sm md:ring-1 md:ring-gray-200 md:hover:ring-gray-300 lg:w-96"
                 @click="searchModalVisible = !searchModalVisible"
             >
-                <svg aria-hidden="true" viewBox="0 0 20 20" class="h-5 w-5 flex-none fill-gray-400 group-hover:fill-gray-500 md:group-hover:fill-gray-400 dark:fill-gray-500">
-                    <path d="M16.293 17.707a1 1 0 0 0 1.414-1.414l-1.414 1.414ZM9 14a5 5 0 0 1-5-5H2a7 7 0 0 0 7 7v-2ZM4 9a5 5 0 0 1 5-5V2a7 7 0 0 0-7 7h2Zm5-5a5 5 0 0 1 5 5h2a7 7 0 0 0-7-7v2Zm8.707 12.293-3.757-3.757-1.414 1.414 3.757 3.757 1.414-1.414ZM14 9a4.98 4.98 0 0 1-1.464 3.536l1.414 1.414A6.98 6.98 0 0 0 16 9h-2Zm-1.464 3.536A4.98 4.98 0 0 1 9 14v2a6.98 6.98 0 0 0 4.95-2.05l-1.414-1.414Z"></path>
-                </svg>
-                <span class="sr-only md:not-sr-only md:ml-2 md:text-gray-500 md:dark:text-gray-400">Search support docs</span>
+                <MagnifyingGlass />
+                <span class="sr-only md:not-sr-only md:ml-2 md:text-gray-500">Search support docs</span>
             </button>
             <Teleport to="body">
                 <SearchModal 
@@ -41,3 +27,16 @@ const searchModalVisible = ref(false)
         </div>
     </header>
 </template>
+
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+import ApplicationLogo from './ApplicationLogo.vue';
+import ProductHomeLink from './ProductHomeLink.vue';
+import { ref } from 'vue';
+import SearchModal from './SearchModal.vue';
+import MagnifyingGlass from './Icons/MagnifyingGlass.vue';
+
+const currentPath = ref(window.location.href)
+
+const searchModalVisible = ref(false)
+</script>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -8,9 +8,7 @@
                 class="group flex h-12 w-72 rounded-md items-center justify-center sm:justify-start md:h-auto md:w-80 md:flex-none md:rounded-lg md:py-2.5 md:pl-4 md:pr-3.5 md:text-sm md:ring-1 md:ring-gray-200 md:hover:ring-gray-300 lg:w-96 bg-white"
                 @click="searchModalVisible = !searchModalVisible"
             >
-                <svg aria-hidden="true" viewBox="0 0 20 20" class="h-5 w-5 flex-none fill-gray-400 group-hover:fill-gray-500 md:group-hover:fill-gray-400">
-                    <path d="M16.293 17.707a1 1 0 0 0 1.414-1.414l-1.414 1.414ZM9 14a5 5 0 0 1-5-5H2a7 7 0 0 0 7 7v-2ZM4 9a5 5 0 0 1 5-5V2a7 7 0 0 0-7 7h2Zm5-5a5 5 0 0 1 5 5h2a7 7 0 0 0-7-7v2Zm8.707 12.293-3.757-3.757-1.414 1.414 3.757 3.757 1.414-1.414ZM14 9a4.98 4.98 0 0 1-1.464 3.536l1.414 1.414A6.98 6.98 0 0 0 16 9h-2Zm-1.464 3.536A4.98 4.98 0 0 1 9 14v2a6.98 6.98 0 0 0 4.95-2.05l-1.414-1.414Z"></path>
-                </svg>
+                <MagnifyingGlass />
                 <span class="text-lg sm:ml-2 text-gray-500">Search support library...</span>
             </button>
             <Teleport to="body">
@@ -44,6 +42,7 @@
 </template>
 
 <script setup lang="ts">
+import MagnifyingGlass from '@/Components/Icons/MagnifyingGlass.vue';
 import PageHeader from '@/Components/PageHeader.vue';
 import SearchModal from '@/Components/SearchModal.vue';
 import MainLayout from '@/Layouts/MainLayout.vue';
