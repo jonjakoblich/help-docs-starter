7577c25198b280f45c63c05ed27940ef6c8dd41f
Jon Jakoblich <jonjakoblich@me.com>
2024-10-27 12:37:42 -0500

    Remove unneeded Jetstream files

--- routes/web.php
+++ routes/web.php
@@ -5,29 +5,7 @@ use App\Http\Controllers\ArticleSearchController;
 use App\Http\Controllers\CastHelpfulVoteController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomePageController;
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-
-/* Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-}); */
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 Route::get('/', HomePageController::class)->name('home');
 
@@ -37,5 +15,3 @@ Route::post('/article/{article:slug}/vote', CastHelpfulVoteController::class)->n
 Route::get('/category/{category:slug}', CategoryController::class)->name('category.view');
 
 Route::post('/search', ArticleSearchController::class)->name('search.retrieve');
-
-require __DIR__.'/auth.php';
