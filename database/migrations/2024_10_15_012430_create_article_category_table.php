e3ed5bbac0f03912b0b76f75faa1b234653d2afb
Jon Jakoblich <jonjakoblich@me.com>
2024-10-29 12:11:35 -0500

    Remove timestamps

--- database/migrations/2024_10_15_012430_create_article_category_table.php
+++ database/migrations/2024_10_15_012430_create_article_category_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->foreignIdFor(Article::class);
             $table->foreignIdFor(Category::class);
-            $table->timestamps();
         });
     }
 
