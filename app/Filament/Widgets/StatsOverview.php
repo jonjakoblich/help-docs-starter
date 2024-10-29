e9c13e016137f4444f946b94c79eaae3f9916a53
Jon Jakoblich <jonjakoblich@me.com>
2024-10-29 12:08:35 -0500

    Add helpfulness stat

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Article;
+use App\Models\HelpfulVote;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -13,6 +14,19 @@ class StatsOverview extends BaseWidget
         return [
             Stat::make('Published Articles', Article::query()->where('status','published')->count()),
             Stat::make('Draft Articles', Article::query()->where('status','draft')->count()),
+            Stat::make('Overall Helpfulness', $this->calculateHelpfulnessScore().'%')
+                ->description('found the articles helpful'),
         ];
+    }
+
+    private function calculateHelpfulnessScore(): int
+    {
+        $allVotes = HelpfulVote::all();
+        
+        $totalVotes = $allVotes->count();
+        
+        $helpfulVotes = $allVotes->where('vote', true)->count();
+
+        return $helpfulVotes/$totalVotes*100;
     }
 }
