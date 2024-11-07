447f8ba72db33c7f8c246b167240ea81d344cb3f
Jon Jakoblich <jonjakoblich@me.com>
2024-11-07 12:00:57 -0600

    Allow adding id attribute to HTML elements

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,9 @@ use App\Exceptions\Handler;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\HtmlSanitizer\HtmlSanitizer;
+use Symfony\Component\HtmlSanitizer\HtmlSanitizerConfig;
+use Symfony\Component\HtmlSanitizer\HtmlSanitizerInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,6 +20,24 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(
             ExceptionHandler::class,
             Handler::class
+        );
+
+        /**
+         * Overrides the default sanitizer implemented by Filament. This was done to allow 
+         * the 'id' attribute on HTML elements in the RichEditor form component.
+         */
+        $this->app->scoped(
+            HtmlSanitizerInterface::class,
+            fn (): HtmlSanitizer => new HtmlSanitizer(
+                (new HtmlSanitizerConfig)
+                    ->allowSafeElements()
+                    ->allowRelativeLinks()
+                    ->allowRelativeMedias()
+                    ->allowAttribute('class', allowedElements: '*')
+                    ->allowAttribute('style', allowedElements: '*')
+                    ->allowAttribute('id', allowedElements: '*')
+                    ->withMaxInputLength(500000),
+            ),
         );
     }
 
